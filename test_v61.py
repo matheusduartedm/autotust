b8434a692dee12413c8c87f06e31be61160e77e6
matheuspsr <113726352+matheuspsr@users.noreply.github.com>
2023-07-28 15:51:32 -0300

    Update

--- test_v61.py
+++ test_v61.py
@@ -2,7 +2,7 @@ import autotust_v61
 
 #SEMPRE RODAR O SCRIPT COMO ADMINISTRADOR
 
-CASE_PATH = r"D:\dev\auto_tust\tests\BaseONS"
+CASE_PATH = r"BaseONS"
 
 rap23 = [41244217.19, 42551207.39, 43105639.57, 46475182.88, 46449217.35, 47993366.19, 48134649.99, 48204543.25]
 pdr23 = [80, 70, 60, 50, 50, 50, 50, 50]
@@ -10,7 +10,5 @@ pdr23 = [80, 70, 60, 50, 50, 50, 50, 50]
 #autotust_v61.run_nodal61(CASE_PATH, rap23, pdr23)
 
 generators, r61_data = autotust_v61.load_base(CASE_PATH)
-
-print(generators, r61_data)
 
 autotust_v61.create_dashboard(generators, r61_data)
