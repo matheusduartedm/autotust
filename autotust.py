931fbcc9d41cd275683c5dd9a0946d95ab7e5068
matheuspsr <113726352+matheuspsr@users.noreply.github.com>
2024-04-30 15:56:41 -0300

    Add output command

--- autotust.py
+++ autotust.py
@@ -4,6 +4,221 @@ import subprocess
 import argparse
 
 
+NODAL_PATH = r"C:\Program Files (x86)\Nodal_V61"
+INITIAL_CYCLE = 2024
+FINAL_CYCLE = 2032
+
+
+class Generator:
+    def __init__(self):
+        self.type = ""
+        self.name = ""
+        self.must = {}
+        self.s = {}
+        self.d = {}
+        self.ceg = ""
+        self.cegnucleo = 0
+        self.bus = {}
+        self.tust = {}
+        self.uf = ""
+
+
+class BusOns:
+    def __init__(self):
+        self.num = 0
+        self.name = ""
+        self.area = 0
+        self.circuits = {}
+        self.tust = {}
+
+
+class BusEpe:
+    def __init__(self):
+        self.num = 0
+        self.name = ""
+        self.area = 0
+        self.circuits = {}
+        self.tust = {}
+
+
+def _is_end_of_file(line):
+    return line[:2].strip() == "X"
+
+
+def _is_comment(line):
+    return len(line) <= 1 or line[0] == "("
+
+
+def _load_r61_data(r61_file):
+    data = {}
+    with open(r61_file, 'r', encoding='ISO-8859-1') as file:
+        lines = file.readlines()
+        # Extract rap
+        rap_total_str = lines[7][88:106].replace(".", "").replace(",", ".")
+        data["rap"] = float(rap_total_str) if rap_total_str.strip() not in ["", "------"] else None
+        # Extract mustg
+        must_ger_str = lines[8][15:26].replace(".", "").replace(",", ".")
+        data["mustg"] = float(must_ger_str) if must_ger_str.strip() not in ["", "------"] else None
+        # Extract mustp
+        must_cp_str = lines[8][55:66].replace(".", "").replace(",", ".")
+        data["mustp"] = float(must_cp_str) if must_cp_str.strip() not in ["", "------"] else None
+        # Extract mustfp
+        must_fp_str = lines[8][75:86].replace(".", "").replace(",", ".")
+        data["mustfp"] = float(must_fp_str) if must_fp_str.strip() not in ["", "------"] else None
+        # Extract TEU
+        teu_values = []
+        for i in range(17, 20):
+            if i < len(lines):
+                teu_str = lines[i][7:14].replace(",", ".")
+                teu_values.append(float(teu_str) if teu_str.strip().strip('-') != "" else None)
+
+            else:
+                teu_values.append(None)
+
+        if len(lines) < 20:
+            teu_values.append(None)
+        data["teug"] = teu_values[0]
+        data["teup"] = teu_values[1]
+        data["teufp"] = teu_values[2]
+
+    return data
+
+
+def load_base(DB_PATH):
+    generators = {}
+    buses = {}
+    r61_data = {
+        "rap": {},
+        "mustg": {},
+        "mustp": {},
+        "mustfp": {},
+        "teug": {},
+        "teup": {},
+        "teufp": {},
+    }
+    years = range(2022, 2032)
+    for year in years:
+        ger_file = DB_PATH + f"\\{year}-{year+1}.GER"
+        tuh_file = DB_PATH + f"\\{year}-{year+1}.TUH"
+        r61_file = DB_PATH + f"\\{year}-{year+1}.R61"
+        nos_file = DB_PATH + f"\\{year}-{year+1}.NOS"
+
+        if os.path.exists(ger_file):
+            with open(ger_file, "r") as file:
+                for line in file:
+                    if _is_comment(line):
+                        continue
+
+                    if len(line) > 0:
+                        name = line[0:32].strip()
+                        if len(line) > 0:
+                            name = line[0:32].strip()
+                            if name not in generators:
+                                generator = Generator()
+                                generator.name = name
+                                generator.type = name[0:3]
+                                generator.ceg = line[52:62].strip()
+                                if generator.ceg[-7:][:5].strip():
+                                    generator.cegnucleo = int(generator.ceg[-7:][:5]) if generator.ceg[-7:][:5].isdigit() else generator.ceg[-7:][:5]
+                                generators[name] = generator
+                            generators[name].d[year] = line[42:43].strip()
+                            generators[name].s[year] = line[44:45].strip()
+                            generators[name].must[year] = float(line[32:41].strip())
+                            generators[name].bus[year] = int(line[70:75].strip())
+                print(f"Geradores carregados para o ano {year}-{year+1}.")
+
+
+        if os.path.exists(tuh_file):
+            with open(tuh_file, "r") as file:
+                line_number = 0
+
+                for line in file:
+                    line_number += 1
+
+                    if line_number <= 11:
+                        continue
+
+                    if _is_comment(line):
+                        continue
+
+                    if _is_end_of_file(line):
+                        break
+
+                    if len(line) > 0:
+                        name = line[3:35].strip()
+                        tust = float(line[96:102])
+                        uf = line[51:53].strip()
+                        if name in generators:
+                            generator = generators[name]
+                            generators[name].tust[year] = tust
+                            generators[name].uf = generators[name].uf if generators[name].uf else uf
+
+                        else:
+                            print(f"Gerador {name} não encontrado na lista de geradores.")
+
+        if os.path.exists(r61_file):
+            year_data = _load_r61_data(r61_file)
+            for key in r61_data.keys():
+                r61_data[key][year] = year_data[key]
+
+        if os.path.exists(nos_file):
+            with open(nos_file, "r") as file:
+                line_number = 0
+
+                for line in file:
+                    line_number += 1
+
+                    if line_number <= 11:
+                        continue
+
+                    if _is_comment(line):
+                        continue
+
+                    if _is_end_of_file(line):
+                        break
+
+                    if len(line) > 0:
+                        name = line[8:21].strip()
+                        tust = float(line[37:43].strip()) if line[37:43].strip() else 0
+                        if name not in buses:
+                            bus = BusOns() if year <= 2026 else BusEpe()
+                            bus.num = int(line[2:7].strip())
+                            bus.name = line[8:20].strip()
+                            bus.tust[year] = tust
+                            buses[name] = bus
+                        else:
+                            buses[name].tust[year] = tust
+
+    # Ordenar os geradores por nome
+    generators = sorted(generators.items(), key=lambda x: x[0])
+    generators = dict(generators)
+
+    # Adicionar anos extras para o dashboard
+    extra_years = range(2032, 2040)
+    for name in generators:
+        generator = generators[name]
+        for year in extra_years:
+            generator.tust[year] = generator.tust.get(year-1, 0) - 0.02
+
+    combined_years = list(years) + list(extra_years)
+
+    # Criar o arquivo CSV
+    csv_file = DB_PATH + "\\" + "autotust_list.csv"
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        header = ["GENERATOR"] + [str(year) for year in combined_years]
+        writer.writerow(header)
+
+        for name, generator in generators.items():
+            row = [name]
+            for year in combined_years:
+                tust = generator.tust.get(year, 0)
+                row.extend([tust])
+            writer.writerow(row)
+
+    return generators, buses, r61_data
+
+
 def _read_param61(file_path):
     param_list = []
     if os.path.exists(file_path):
@@ -46,18 +261,14 @@ def run_nodal61(case_path,
                 rap=[41244217.19, 42551207.39, 43105639.57, 46475182.88, 46449217.35, 47993366.19, 48134649.99,
                      48204543.25],
                 pdr=[80, 70, 60, 50, 50, 50, 50, 50]):
-    NODAL_PATH = r"C:\Program Files (x86)\Nodal_V61"
-    cycle_begin = 2024
-    cycle_end = 2032
-    working_path = NODAL_PATH
     params_file_path = os.path.join(NODAL_PATH, "param.v61")
 
     """ 	if not os.exists(params_file_path):
-        create_param_file(params_file_path, case_path, cycle_begin) """
+        create_param_file(params_file_path, case_path, INITIAL_CYCLE) """
 
     params = _read_param61(params_file_path)
     params[1] = NODAL_PATH
-    for i, icycle in enumerate(range(cycle_begin, cycle_end)):
+    for i, icycle in enumerate(range(INITIAL_CYCLE, FINAL_CYCLE)):
         cycle = f"{icycle}-{icycle + 1}"
         params[2] = case_path + f"\{cycle}.dc"
         params[3] = case_path + f"\{cycle}"
@@ -78,7 +289,7 @@ def run_nodal61(case_path,
 
 def read_autotust_csv(csv_path):
     with open(csv_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter='\t')
+        reader = csv.DictReader(csvfile)
         rap_values, pdr_values = [], []
         for row in reader:
             rap_values.append(float(row['rap']))
@@ -86,11 +297,50 @@ def read_autotust_csv(csv_path):
     return rap_values, pdr_values
 
 
+def get_tust_results(case_path):
+    ss = {'AP': 'N', 'AM': 'N', 'PA': 'N', 'RR': 'N', 'TO': 'N', 'MA': 'N',
+          'AL': 'NE', 'BA': 'NE', 'CE': 'NE', 'PB': 'NE', 'PE': 'NE', 'PI': 'NE', 'RN': 'NE', 'SE': 'NE',
+          'ES': 'SE', 'MG': 'SE', 'RJ': 'SE', 'SP': 'SE', 'GO': 'SE', 'MS': 'SE', 'MT': 'SE', 'DF': 'SE', 'AC': 'SE',
+          'RO': 'SE',
+          'PR': 'S', 'RS': 'S', 'SC': 'S'}
+
+    generators, _, _ = load_base(case_path)
+
+    years = range(2023, 2032)
+    with open(os.path.join(case_path, "autotust_results.csv"), mode='w', newline='') as file:
+        writer = csv.writer(file)
+        header = ["USINA", "SUBSISTEMA", "CEG"] + [str(year) for year in years] + ["MUST", "BUS_2023", "BUS_2030"]
+        writer.writerow(header)
+
+        for name, generator in generators.items():
+            row = [name]
+
+            subsistema = ss.get(generator.uf, '')
+            row.extend([subsistema, generator.ceg])
+
+            for year in years:
+                tust_year = generator.tust.get(year, '-')
+                row.extend([tust_year])
+
+            must = generator.must.get(2030, '-')
+            row.extend([must])
+            bus23 = generator.bus.get(2023, '-')
+            row.extend([bus23])
+            bus30 = generator.bus.get(2030, '-')
+            row.extend([bus30])
+
+            writer.writerow(row)
+
+
 def main():
     parser = argparse.ArgumentParser(description="AutoTUST - Nodal Automation v1.0")
     subparsers = parser.add_subparsers(dest='command', help='commands')
+
     nodal_parser = subparsers.add_parser('nodal', help='Run Nodal v61')
     nodal_parser.add_argument('path', type=str, help='Path to the case folder')
+
+    output_parser = subparsers.add_parser('output', help='Get TUST results')
+    output_parser.add_argument('path', type=str, help='Path to the case folder')
 
     args = parser.parse_args()
 
@@ -98,6 +348,9 @@ def main():
         csv_path = os.path.join(args.path, "autotust.csv")
         rap, pdr = read_autotust_csv(csv_path)
         run_nodal61(args.path, rap, pdr)
+
+    elif args.command == 'output':
+        get_tust_results(args.path)
 
 
 if __name__ == "__main__":
